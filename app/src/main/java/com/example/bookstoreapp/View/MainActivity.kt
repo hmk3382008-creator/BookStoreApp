09a765e3e37e923f48a1dd04f127ad9a05afa8c4
Ziv <hmk3382008@gmail.com>
2025-12-09 09:58:10 +0800

    xml

--- app/src/main/java/com/example/bookstoreapp/View/MainActivity.kt
+++ app/src/main/java/com/example/bookstoreapp/View/MainActivity.kt
@@ -21,11 +21,11 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+////            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+////            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+////            insets
+//        }
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
